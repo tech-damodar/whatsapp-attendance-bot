924167872b012906c886e77e08fe8d8c36f0ecb6
Damodar Swami <damodarswami7@gmail.com>
2026-03-11 19:37:34 +0530

    first commit

---
+++ auto.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const app = express();
+app.use(express.json());
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on("qr", qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on("ready", () => {
+    console.log("WhatsApp Bot Ready");
+});
+
+client.initialize();
+
+app.post("/send", async (req,res)=>{
+
+    const message = req.body.message;
+
+    const groupId = "120363422672209611@g.us";
+
+    await client.sendMessage(groupId,message);
+
+    res.send("Message sent");
+
+});
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000");
+});
