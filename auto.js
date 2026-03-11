bb3f52c64e984ce9eea3cb5a1ae225560912ef93
Damodar Swami <damodarswami7@gmail.com>
2026-03-11 20:37:12 +0530

    fix port

--- auto.js
+++ auto.js
@@ -31,6 +31,8 @@ app.post("/send", async (req,res)=>{
 
 });
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
